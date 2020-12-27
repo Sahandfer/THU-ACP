c3c935d22a12048a9fa7eff6bcdc30beb809f5fa
Sahandfer <karimiali0022@yahoo.com>
2020-12-27 19:18:13 +0800

    Updated utils

--- Machine Learning/Assignments/HW3/Code/utils.py
+++ Machine Learning/Assignments/HW3/Code/utils.py
@@ -1,51 +1,25 @@
-import gzip
 import numpy as np
-import os
-from six.moves import cPickle as pickle
+from skimage import io, img_as_ubyte
+from skimage.exposure import rescale_intensity
+from tensorflow.examples.tutorials.mnist import input_data
 
+def shuffle(x, l):
+    randIndx = np.arange(len(x))
+    np.random.shuffle(randIndx)
+    x, l = x[randIndx], l[randIndx]
+    
+    return x, l
 
-def makedirs(filename):
-    if not os.path.exists(os.path.dirname(filename)):
-        os.makedirs(os.path.dirname(filename))
-        
-def to_one_hot(arr, depth):
-    arr_temp = np.zeros((arr.shape[0], depth))
-    arr_temp[np.arange(arr.shape[0]), arr] = 1
-    return arr_temp
-
-
+# Load the MNIST dataset in binary form (one-hot)
 def load_dataset():
-    data_file = gzip.open("mnist.pkl.gz", "rb")
-    train_data, val_data, test_data = pickle.load(data_file, encoding="latin1")
-    # Decompose the images and corresponding labels
-    train_image, train_label = train_data
-    test_image, test_label = test_data
+    mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
+    x_train, l_train = mnist.train.images, mnist.train.labels
 
-    depth = train_label.max()+1
-    train_label = to_one_hot(train_label, depth) 
-    test_label = to_one_hot(test_label, depth)
+    return x_train, l_train
 
-    return train_image, train_label, test_image, test_label
 
-def save_image_collections(x, filename, shape=(10, 10), scale_each=False,
-                           transpose=False):
-    """
-    :param shape: tuple
-        The shape of final big images.
-    :param x: numpy array
-        Input image collections. (number_of_images, rows, columns, channels) or
-        (number_of_images, channels, rows, columns)
-    :param scale_each: bool
-        If true, rescale intensity for each image.
-    :param transpose: bool
-        If true, transpose x to (number_of_images, rows, columns, channels),
-        i.e., put channels behind.
-    :return: `uint8` numpy array
-        The output image.
-    """
-    from skimage import io, img_as_ubyte
-    from skimage.exposure import rescale_intensity
-    makedirs(filename)
+# Save a generated image (imported from zhusuan/example)
+def save_image(x, filename, shape=(10, 10), scale_each=False, transpose=False):
     n = x.shape[0]
     if transpose:
         x = x.transpose(0, 2, 3, 1)
@@ -56,12 +30,12 @@ def save_image_collections(x, filename, shape=(10, 10), scale_each=False,
     x = img_as_ubyte(x)
     r, c = shape
     if r * c < n:
-        print('Shape too small to contain all images')
+        print("Shape too small to contain all images")
     h, w = x.shape[1:3]
-    ret = np.zeros((h * r, w * c, n_channels), dtype='uint8')
+    ret = np.zeros((h * r, w * c, n_channels), dtype="uint8")
     for i in range(r):
         for j in range(c):
             if i * c + j < n:
-                ret[i * h:(i + 1) * h, j * w:(j + 1) * w, :] = x[i * c + j]
+                ret[i * h : (i + 1) * h, j * w : (j + 1) * w, :] = x[i * c + j]
     ret = ret.squeeze()
     io.imsave(filename, ret)
