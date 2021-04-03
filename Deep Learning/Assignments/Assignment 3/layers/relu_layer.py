94ad72edab670a6ae63c8af6a00038d3012667c1
Sahandfer <karimiali0022@yahoo.com>
2021-04-03 18:35:45 +0800

    Added layers from A2

--- Deep Learning/Assignments/Assignment 3/layers/relu_layer.py
+++ Deep Learning/Assignments/Assignment 3/layers/relu_layer.py
@@ -2,28 +2,28 @@
 
 import numpy as np
 
-class ReLULayer():
-	def __init__(self):
-		"""
-		Applies the rectified linear unit function element-wise: relu(x) = max(x, 0)
-		"""
-		self.trainable = False # no parameters
 
-	def forward(self, Input, **kwargs):
+class ReLULayer:
+    def __init__(self):
+        """
+        Applies the rectified linear unit function element-wise: relu(x) = max(x, 0)
+        """
+        self.trainable = False  # no parameters
 
-		############################################################################
-	    # TODO: Put your code here
-		# Apply ReLU activation function to Input, and return results.
+    def forward(self, Input, **kwargs):
 
+        ############################################################################
+        # TODO: Put your code here
+        # Apply ReLU activation function to Input, and return results.
+        Input[Input < 0] = 0
+        self.output = Input
+        return self.output
+        ############################################################################
 
-	    ############################################################################
+    def backward(self, delta):
 
-
-	def backward(self, delta):
-
-		############################################################################
-	    # TODO: Put your code here
-		# Calculate the gradient using the later layer's gradient: delta
-
-
-	    ############################################################################
+        ############################################################################
+        # TODO: Put your code here
+        # Calculate the gradient using the later layer's gradient: delta
+        return delta * (self.output > 0)
+        ############################################################################
--- Deep Learning/Assignments/Assignment 3/layers/sigmoid_layer.py
+++ Deep Learning/Assignments/Assignment 3/layers/sigmoid_layer.py
@@ -2,27 +2,27 @@
 
 import numpy as np
 
-class SigmoidLayer():
-	def __init__(self):
-		"""
-		Applies the element-wise function: f(x) = 1/(1+exp(-x))
-		"""
-		self.trainable = False
 
-	def forward(self, Input, **kwargs):
+class SigmoidLayer:
+    def __init__(self):
+        """
+        Applies the element-wise function: f(x) = 1/(1+exp(-x))
+        """
+        self.trainable = False
 
-		############################################################################
-	    # TODO: Put your code here
-		# Apply Sigmoid activation function to Input, and return results.
+    def forward(self, Input, **kwargs):
 
+        ############################################################################
+        # TODO: Put your code here
+        # Apply Sigmoid activation function to Input, and return results.
+        self.output = 1.0 / (1.0 + np.exp(-Input))
+        return self.output
+        ############################################################################
 
-	    ############################################################################
+    def backward(self, delta):
 
-	def backward(self, delta):
-
-		############################################################################
-	    # TODO: Put your code here
-		# Calculate the gradient using the later layer's gradient: delta
-
-
-	    ############################################################################
+        ############################################################################
+        # TODO: Put your code here
+        # Calculate the gradient using the later layer's gradient: delta
+        return delta * self.output * (1 - self.output)
+        ############################################################################
