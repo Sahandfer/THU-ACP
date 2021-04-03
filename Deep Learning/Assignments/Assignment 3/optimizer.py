9191284774410084cd99ab7182ece7a7175a2d73
Sahandfer <karimiali0022@yahoo.com>
2021-04-03 18:36:19 +0800

    Added other files from A2

--- Deep Learning/Assignments/Assignment 3/optimizer.py
+++ Deep Learning/Assignments/Assignment 3/optimizer.py
@@ -2,25 +2,28 @@
 
 import numpy as np
 
-class SGD():
-	def __init__(self, learningRate, weightDecay):
-		self.learningRate = learningRate
-		self.weightDecay = weightDecay
 
-	# One backpropagation step, update weights layer by layer
-	def step(self, model):
-		layers = model.layerList
-		for layer in layers:
-			if layer.trainable:
+class SGD:
+    def __init__(self, learningRate, weightDecay):
+        self.learningRate = learningRate
+        self.weightDecay = weightDecay
 
-				############################################################################
-			    # TODO: Put your code here
-				# Calculate diff_W and diff_b using layer.grad_W and layer.grad_b.
-				# Do not forget the weightDecay term.
+    # One backpropagation step, update weights layer by layer
+    def step(self, model):
+        layers = model.layerList
+        for layer in layers:
+            if layer.trainable:
 
+                ############################################################################
+                # TODO: Put your code here
+                # Calculate diff_W and diff_b using layer.grad_W and layer.grad_b.
+                # Do not forget the weightDecay term.
+                layer.diff_W = -self.learningRate * (
+                    layer.grad_W + self.weightDecay * layer.W
+                )
+                layer.diff_b = -self.learningRate * layer.grad_b
+                ############################################################################
 
-			    ############################################################################
-
-				# Weight update
-				layer.W += layer.diff_W
-				layer.b += layer.diff_b
+                # Weight update
+                layer.W += layer.diff_W
+                layer.b += layer.diff_b
