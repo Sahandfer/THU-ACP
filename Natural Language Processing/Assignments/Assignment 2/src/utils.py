066a09eeabfd8967b77bc5ff31b6a56e0418952a
Sahandfer <karimiali0022@yahoo.com>
2021-04-13 09:53:58 +0800

    Removed gensim dict

--- Natural Language Processing/Assignments/Assignment 2/src/utils.py
+++ Natural Language Processing/Assignments/Assignment 2/src/utils.py
@@ -1,30 +1,13 @@
 import os
 import pickle
-from gensim import corpora
-
-
-def save_dict(dir_name, file_name, vocab_dict):
-    try:
-        os.makedirs(dir_name)
-    except Exception:
-        pass
-    finally:
-        vocab_dict.save(f"{dir_name}/{file_name}")
-        print(f"**Saved dictionary**")
-
-
-def load_dict(dir_name, file_name):
-    print(f"**Loading cached dictionary**")
-    return corpora.Dictionary.load(f"{dir_name}/{file_name}")
 
 
 def load_pickle(dir_name, file_name):
     try:
         with open(f"{dir_name}/{file_name}", "rb") as f:
-            # print(f"**Loading cached {dir_name}/{file_name}**")
+            print(f"**Loaded cached {dir_name}/{file_name}**")
             return pickle.load(f)
     except Exception:
-        print("not found")
         return {}
 
 
@@ -34,6 +17,6 @@ def save_pickle(dir_name, file_name, data):
     except Exception:
         pass
     finally:
-        with open(f"{dir_name}/{file_name}.pickle", "wb") as f:
+        with open(f"{dir_name}/{file_name}", "wb") as f:
             pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
             print(f"**Saved {dir_name}/{file_name}**")
