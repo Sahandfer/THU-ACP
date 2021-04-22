82012bd184da3e6cdebbd40e716882b8ab21d462
Sahandfer <karimiali0022@yahoo.com>
2021-04-22 19:33:31 +0800

    Started HW2

---
+++ Natural Language Processing/Assignments/Assignment 3/main.py
@@ -0,0 +1,113 @@
+import os
+import re
+import nltk
+import argparse
+import pandas as pd
+from tqdm import tqdm
+from torchtext.legacy import data
+from torchtext.vocab import Vectors
+
+from src.model import LSTM
+from src.train import train
+from src.test import test
+
+
+class Args:
+    def __init__(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--batch_size", default=64, type=int)
+        parser.add_argument("--num_epochs", default=10, type=int)
+        parser.add_argument("--num_layers", default=2, type=int)
+        parser.add_argument("--hidden_dim", default=512, type=int)
+        parser.add_argument("--learning_rate", default=0.001, type=float)
+        parser.add_argument("--dropout", default=0.2, type=float)
+        parser.add_argument("--use_embed", default=False, action="store_true")
+
+        self.parser = parser
+
+    def get_args(self):
+        return self.parser.parse_args()
+
+
+def read_data():
+    data_dir = "trees/"
+    files = ["train.txt", "dev.txt", "test.txt"]
+    out_files = ["train.csv", "dev.csv", "test.csv"]
+    for i, file in enumerate(files):
+        sentences = []
+        labels = []
+        for line in tqdm(open(data_dir + file)):
+            tokenizer = nltk.RegexpTokenizer(r"\w+")
+            # Remove digits
+            sentence = re.sub("\d+", "", line)
+            # Remove non-words
+            sentence = tokenizer.tokenize(sentence)
+            # Re-create the sentence
+            sentence = " ".join(sentence)
+            sentences.append(sentence)
+
+            # Target label is the first digit in the line
+            label = line[1]
+            labels.append(label)
+
+        # Save files to CSV for torchtext tabular dataset.
+        df = pd.DataFrame(list(zip(sentences, labels)), columns=["sentence", "label"])
+        df.to_csv(data_dir + out_files[i], index=False)
+
+
+def preprocess_data(args):
+    # The input data has two attributes -> text and label (sentiment)
+    SENTENCE = data.Field(
+        lower=True,
+        sequential=True,
+        batch_first=True,
+        eos_token="<eos>",
+        init_token="<sos>",
+        include_lengths=True,
+        tokenize=nltk.word_tokenize,
+    )
+    LABEL = data.Field(sequential=False, use_vocab=False, is_target=True)
+
+    # Split data into training, validation, and testing sets
+    fields = [("sentence", SENTENCE), ("label", LABEL)]
+    train_set, val_set, test_set = data.TabularDataset.splits(
+        path="./trees",
+        format="csv",
+        train="train.csv",
+        validation="dev.csv",
+        test="test.csv",
+        fields=fields,
+        skip_header=True,
+    )
+
+    print(
+        f">>> Created Sets -> {len(train_set)} Training - {len(val_set)} Validation - {len(test_set)} Test"
+    )
+
+    # Build the vocabulary from pre-trained embeddings
+    SENTENCE.build_vocab(
+        train_set, vectors=Vectors(name="glove.6B.300d.txt", cache="./")
+    )
+    LABEL.build_vocab(train_set)
+    num_words, embed_dim = SENTENCE.vocab.vectors.size()
+    print(f">>> Created Vocabulary Dictionary")
+    print(f"| # Words: {num_words} --- Embedding Dim: {embed_dim} |")
+
+    # Create batch iterators for the datasets
+    train_iter, val_iter, test_iter = data.BucketIterator.splits(
+        (train_set, val_set, test_set), batch_size=args.batch_size
+    )
+    print(f">>> Created Batch Iterators with size {args.batch_size}")
+
+    return SENTENCE, train_iter, val_iter, test_iter
+
+
+def main():
+    args = Args().get_args()
+    if not (os.path.exists("trees/dev.csv")):
+        read_data()  # Create CSV files if they don't exists
+    SENTENCE, train_iter, val_iter, test_iter = preprocess_data(args)
+
+
+if __name__ == "__main__":
+    main()
